c475f0a3c53ee8e4e430e1d9cb133dc0ba392341
ronnkorn.K <ronnakornb@metis.co.th>
2025-05-06 02:23:47 +0700

    no message

--- homecarre-frontend/src/components/RootConfig.jsx
+++ homecarre-frontend/src/components/RootConfig.jsx
@@ -1,15 +1,10 @@
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { AuthProvider } from "@/context/AuthContext";
-import { UserProvider } from "@/context/userContext";
 
 const RootConfig = ({ children }) => {
   return (
     <AuthProvider>
-      {/* <UserProvider> */}
-        <AntdRegistry>
-          {children}
-        </AntdRegistry>
-      {/* </UserProvider> */}
+      <AntdRegistry>{children}</AntdRegistry>
     </AuthProvider>
   );
 };
