f9149bb5f899abd3b4282e2ad2ed353eabf2bf0a
ronnkorn.K <ronnakornb@metis.co.th>
2025-05-27 17:15:54 +0700

    no message

--- homecarre-frontend/src/services/Auth/logInService.js
+++ homecarre-frontend/src/services/Auth/logInService.js
@@ -3,6 +3,7 @@ import api from "@/util/api";
 const LogInService = async (body) => {
   try {
     const response = await api(`/admin/auth/login`, "POST", body);
+    console.log("login response", response);
     return response;
   } catch (error) {
     console.error("Login error:", error);
